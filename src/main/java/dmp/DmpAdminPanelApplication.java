d20191794b554c31a7bb2f8c0729a958bddcab93
shibli <shibli.shadik@gmail.com>
2019-11-12 12:24:14 +0600

    first commit

---
+++ src/main/java/dmp/DmpAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package dmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmpAdminPanelApplication 
+{
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(DmpAdminPanelApplication.class, args);
+    }
+}
---
+++ src/main/java/dmp/controller/admin/login/LoginController.java
@@ -0,0 +1,36 @@
+package dmp.controller.admin.login;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = {"/",""})
+public class LoginController 
+{
+    @PostMapping("/home")
+    public String validateLoginInfo(Model model, HttpServletRequest request,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password) 
+    {
+        return "home/home";
+    }
+    
+    @GetMapping("/home")
+    public String getHomePageInfo(Model model, HttpServletRequest request) 
+    {
+        return "home/home";
+    }
+    
+    @GetMapping("/logout")
+    public String getLogoutInfo(Model model, HttpServletRequest request) 
+    {
+        //request.getSession().removeAttribute("itclificdciusrsession");
+        
+        return "index";
+    }
+}
